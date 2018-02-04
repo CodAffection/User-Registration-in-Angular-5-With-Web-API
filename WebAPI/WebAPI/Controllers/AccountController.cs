9326bef8a6f407b8ad5445ce8182ed81ce0e947e
DotnetMob <dotnetmob@gmail.com>
2018-02-04 13:10:25 +0530

    WebAPI

---
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IdentityResult Register(AccountModel model)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 3
+            };
+            IdentityResult result = manager.Create(user, model.Password);
+            return result;
+        }
+    }
+}
