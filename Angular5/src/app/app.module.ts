fd1949cd80057f964429e4356d4fc75140445002
DotnetMob <dotnetmob@gmail.com>
2018-02-04 13:08:04 +0530

    angular5

---
+++ Angular5/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserService } from './shared/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular5/src/app/shared/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {  Response } from "@angular/http";
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import { User } from './user.model';
+
+@Injectable()
+export class UserService {
+  readonly rootUrl = 'http://localhost:35257';
+  constructor(private http: HttpClient) { }
+
+  registerUser(user : User){
+    const body: User = {
+      UserName: user.UserName,
+      Password: user.Password,
+      Email: user.Email,
+      FirstName: user.FirstName,
+      LastName: user.LastName
+    }
+    return this.http.post(this.rootUrl + '/api/User/Register', body);
+  }
+
+}
